663c8620bc0f4ed5d72b15a8916f93ed439ebea2
Cuthbert20 <knowltonspencer@gmail.com>
2019-08-08 18:29:33 -0600

    massive and node setup

--- server/server.js
+++ server/server.js
@@ -1,2 +1,20 @@
 const express = require('express')
 require('dotenv').config()
+const app = express()
+const { SESSION_SECRET, SERVER_PORT, CONNECTION_STRING } = process.env
+const massive = require('massive')
+const session = require('express-session')
+
+app.use(express.json())
+
+
+
+
+
+massive(CONNECTION_STRING).then(db => {
+    app.set('db', db)
+    
+    app.listen(SERVER_PORT, () => console.log(`${SERVER_PORT} is now listening Stu`))
+    
+
+}).catch(err => console.log(err, "not hitting database"))
